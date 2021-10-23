c3c30316d74ca600320e9a6264d8aa3704b86999
Ahmad Rasekh <ahmd.rasekh@gmail.com>
2021-10-24 00:05:51 +0100

    State - wip

--- src/Course/State.hs
+++ src/Course/State.hs
@@ -1,18 +1,19 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Course.State where
 
-import Course.Core
-import qualified Prelude as P
-import Course.Optional
-import Course.List
-import Course.Functor
 import Course.Applicative
+import Course.Core
+import Course.Functor
+import Course.List
 import Course.Monad
+import Course.Optional
 import qualified Data.Set as S
+import qualified Prelude as P
 
 -- $setup
 -- >>> import Test.QuickCheck.Function
@@ -24,63 +25,53 @@ import qualified Data.Set as S
 -- >>> instance Arbitrary a => Arbitrary (List a) where arbitrary = P.fmap listh arbitrary
 
 -- A `State` is a function from a state value `s` to (a produced value `a`, and a resulting state `s`).
-newtype State s a =
-  State {
-    runState ::
-      s
-      -> (a, s)
-  }
+newtype State s a = State {runState :: s -> (a, s)}
 
 -- | Run the `State` seeded with `s` and retrieve the resulting state.
 --
 -- prop> \(Fun _ f) s -> exec (State f) s == snd (runState (State f) s)
-exec ::
-  State s a
-  -> s
-  -> s
-exec =
-  error "todo: Course.State#exec"
+exec :: State s a -> s -> s
+exec state s = snd (runState state s)
+
+-- error "todo: Course.State#exec"
 
 -- | Run the `State` seeded with `s` and retrieve the resulting value.
 --
 -- prop> \(Fun _ f) s -> eval (State f) s == fst (runState (State f) s)
-eval ::
-  State s a
-  -> s
-  -> a
-eval =
-  error "todo: Course.State#eval"
+eval :: State s a -> s -> a
+eval sa s = fst (runState sa s)
+
+-- error "todo: Course.State#eval"
 
 -- | A `State` where the state also distributes into the produced value.
 --
 -- >>> runState get 0
 -- (0,0)
-get ::
-  State s s
-get =
-  error "todo: Course.State#get"
+get :: State s s
+get = (\() -> State (\s -> (s, s))) ()
+
+-- error
+-- "todo: Course.State#get"
 
 -- | A `State` where the resulting state is seeded with the given value.
 --
 -- >>> runState (put 1) 0
 -- ((),1)
-put ::
-  s
-  -> State s ()
-put =
-  error "todo: Course.State#put"
+put :: s -> State s ()
+put s = State (\_ -> ((), s))
 
 -- | Implement the `Functor` instance for `State s`.
 --
 -- >>> runState ((+1) <$> State (\s -> (9, s * 2))) 3
 -- (10,6)
 instance Functor (State s) where
-  (<$>) ::
-    (a -> b)
-    -> State s a
-    -> State s b
-  (<$>) =
-    error "todo: Course.State#(<$>)"
+  (<$>) :: (a -> b) -> State s a -> State s b
+  (<$>) f sa =
+    let stateFunction = runState sa
+     in State (\s -> let (a, s') = stateFunction s in (f a, s'))
+
+-- error
+-- "todo: Course.State#(<$>)"
 
 -- | Implement the `Applicative` instance for `State s`.
 --
@@ -93,17 +84,15 @@ instance Functor (State s) where
 -- >>> runState (State (\s -> ((+3), s ++ ("apple":.Nil))) <*> State (\s -> (7, s ++ ("banana":.Nil)))) Nil
 -- (10,["apple","banana"])
 instance Applicative (State s) where
-  pure ::
-    a
-    -> State s a
-  pure =
-    error "todo: Course.State pure#instance (State s)"
-  (<*>) ::
-    State s (a -> b)
-    -> State s a
-    -> State s b
-  (<*>) =
-    error "todo: Course.State (<*>)#instance (State s)"
+  pure :: a -> State s a
+  pure a = State (a,) -- State (\s -> (a, s))
+  (<*>) :: State s (a -> b) -> State s a -> State s b
+  (<*>) sab sa =
+    let x = runState sab
+     in let y = runState sa
+         in State (\s -> let (f, s') = x s in let (a, s'') = y s' in (f a, s''))
+
+-- error "todo: Course.State (<*>)#instance (State s)"
 
 -- | Implement the `Monad` instance for `State s`.
 --
@@ -116,12 +105,15 @@ instance Applicative (State s) where
 -- >>> runState ((\a -> State (\s -> (a + s, 10 + s))) =<< State (\s -> (s * 2, 4 + s))) 2
 -- (10,16)
 instance Monad (State s) where
-  (=<<) ::
-    (a -> State s b)
-    -> State s a
-    -> State s b
-  (=<<) =
-    error "todo: Course.State (=<<)#instance (State s)"
+  (=<<) :: (a -> State s b) -> State s a -> State s b
+  (=<<) f sa =
+    State
+      ( \s ->
+          let (a, s') = runState sa s
+           in runState (f a) s'
+      )
+
+-- error "todo: Course.State (=<<)#instance (State s)"
 
 -- | Find the first element in a `List` that satisfies a given predicate.
 -- It is possible that no element is found, hence an `Optional` result.
@@ -137,11 +129,7 @@ instance Monad (State s) where
 --
 -- >>> let p x = (\s -> (const $ pure (x == 'i')) =<< put (1+s)) =<< get in runState (findM p $ listh ['a'..'h']) 0
 -- (Empty,8)
-findM ::
-  Monad f =>
-  (a -> f Bool)
-  -> List a
-  -> f (Optional a)
+findM :: Monad f => (a -> f Bool) -> List a -> f (Optional a)
 findM =
   error "todo: Course.State#findM"
 
@@ -155,10 +143,7 @@ findM =
 --
 -- prop> \xs -> case firstRepeat xs of Empty -> let xs' = hlist xs in nub xs' == xs'; Full x -> length (filter (== x) xs) > 1
 -- prop> \xs -> case firstRepeat xs of Empty -> True; Full x -> let (l, (rx :. rs)) = span (/= x) xs in let (l2, r2) = span (/= x) rs in let l3 = hlist (l ++ (rx :. Nil) ++ l2) in nub l3 == l3
-firstRepeat ::
-  Ord a =>
-  List a
-  -> Optional a
+firstRepeat :: Ord a => List a -> Optional a
 firstRepeat =
   error "todo: Course.State#firstRepeat"
 
@@ -168,10 +153,7 @@ firstRepeat =
 -- prop> \xs -> firstRepeat (distinct xs) == Empty
 --
 -- prop> \xs -> distinct xs == distinct (flatMap (\x -> x :. x :. Nil) xs)
-distinct ::
-  Ord a =>
-  List a
-  -> List a
+distinct :: Ord a => List a -> List a
 distinct =
   error "todo: Course.State#distinct"
 
@@ -196,8 +178,6 @@ distinct =
 --
 -- >>> isHappy 44
 -- True
-isHappy ::
-  Integer
-  -> Bool
+isHappy :: Integer -> Bool
 isHappy =
   error "todo: Course.State#isHappy"
